ff9b1f4faf1d9de1ca9692ba98aee93de0f02c51
naila-ady <164941279+naila-ady@users.noreply.github.com>
2025-02-06 21:37:52 +0500

    .

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,5 @@
 import { type SchemaTypeDefinition } from 'sanity'
-import product from "@/sanity/schemaTypes/product"
+import product from "../schemaTypes/product"
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [product],
